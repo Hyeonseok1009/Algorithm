e402ae1a3e921674814552bb96e8b9954b8a0ff7
Hyeonseok1009 <76680381+Hyeonseok1009@users.noreply.github.com>
2021-04-21 09:43:02 +0900

    Add files via upload

---
+++ Mycode/quicksort.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+// 디테일은 선택이 아니라 필수다. 반드시 신경써서 기록하여야한다.
+
+int number = 10;
+int data[] = { 9,4,5,8,7,6,2,10,1,3};
+
+void quicksort(int data[], int start, int end) {
+	if (start >= end) { return; }
+
+	int pivot = start;
+	int left = start + 1;
+	int right = end;
+	int tmp = 0;
+	while (left <= right) {			/* 교차하기 전까지*/
+		//printf("A");
+		while (left <= end && data[left] <= data[pivot]) { left++; 
+		}
+		while (right > start && data[right] >= data[pivot]) {
+			right--;
+		}
+		if (left >right) {
+			tmp = data[right];
+			data[right] = data[pivot];
+			data[pivot] = tmp;
+		}
+		else {
+			tmp = data[left];
+			data[left] = data[right];
+			data[right] = tmp;
+		}
+
+		quicksort(data, start, right - 1);
+		quicksort(data, right + 1, end);
+	}
+}
+
+int main() {
+	quicksort(data, 0, number - 1);
+	for (int i = 0; i < number; i++) {
+		printf("%d ", data[i]);
+	}
+	return 0;
+}
